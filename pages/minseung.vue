6969bc513ce7d58712b221c9f31a8c9dbe55d5a4
withseungryu <alstmdsha@gmail.com>
2020-06-10 15:19:59 +0900

    first commit

---
+++ pages/minseung.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Hello world</h2>
+        <h3>{{message}}</h3>
+        {{fullName}}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        data(){
+            return {
+                message : 'Hello World',
+                firstname : 'minseung',
+                lastname : 'Park'
+            }
+        },
+        computed: {
+            fullName (): string {
+                return `${this.firstname} ${this.lastname}`
+            }
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
